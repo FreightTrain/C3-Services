ac9a17abdfaa247f8c95b153f6f777f56675fcaa
Andrew Crump and Matt Dainty <freightrain@cisco>
2014-06-12 11:36:56 +0100

    Don't error if dev release config is unavailable
    [#72680630]

--- lib/bosh_helper.rb
+++ lib/bosh_helper.rb
@@ -15,8 +15,15 @@ module Services
       bosh_mediator = create_bosh_mediator(bosh_state[:director_url], bosh_state[:username], bosh_state[:password], release_dir)
       if bosh_state[:stemcell_resource_uri] && bosh_state[:spiff_dir]
         stemcell_release_info = bosh_mediator.upload_stemcell_to_director(bosh_state[:stemcell_resource_uri])
-        release_manifest = ::BoshMediator::ReleaseManager.new.find_dev_release(release_dir)
-        stemcell_release_info.merge!(:release_version => YAML.load_file(release_manifest)['version'])
+
+        dev_release_config = Pathname.new(release_dir) + 'config' + 'dev.yml'
+        if dev_release_config.exist?
+          release_manifest = ::BoshMediator::ReleaseManager.new.find_dev_release(release_dir)
+          stemcell_release_info.merge!(:release_version => YAML.load_file(release_manifest)['version'])
+        else
+          stemcell_release_info.merge!(:release_version => 'latest')
+        end
+
         manifest_file = ::BoshMediator::ManifestWriter.new(core_manifest, stemcell_release_info, bosh_state[:spiff_dir]).parse_and_merge_file
         bosh_mediator.set_manifest_file(manifest_file)
       end
