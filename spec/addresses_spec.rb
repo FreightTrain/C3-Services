f1911b14e889723cd15a4a1e82ef82d456981ef4
James Masson and Matt Dainty <freighttrain@cisco>
2014-06-19 10:32:42 +0100

    Fix tests now we pass in the YAML file.
    [#72786666]

--- spec/addresses_spec.rb
+++ spec/addresses_spec.rb
@@ -3,13 +3,13 @@ require_relative '../lib/addresses'
 module Services
   describe Addresses do
     it 'loads the environment yaml relative to the spiff directory' do
-      a = Addresses.new('/foo/bar')
-      expect(YAML).to receive(:load_file).with('/foo/bar/env.yml').and_return({'jobs' => []})
+      a = Addresses.new('/foo/bar/baz.yml')
+      expect(YAML).to receive(:load_file).with('/foo/bar/baz.yml').and_return({'jobs' => []})
       a.addresses_for_job('foo')
     end
     it 'returns the static ips associated with a job' do
-      a = Addresses.new('/foo/bar')
-      allow(YAML).to receive(:load_file).with('/foo/bar/env.yml').and_return({
+      a = Addresses.new('/foo/bar/baz.yml')
+      allow(YAML).to receive(:load_file).with('/foo/bar/baz.yml').and_return({
         'jobs' => [
           {
             'name' => 'foo',
